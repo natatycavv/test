0b541dd3b15e9483ca4dee74b556f9aaf1f0d940
Nata <natatycavv@gmail.com>
2020-11-02 13:47:15 +0500

    [+]Добавлена обработка данных в сервисе для получения данных по API

--- src/main/java/com/example/test/controllers/WeatherController.java
+++ src/main/java/com/example/test/controllers/WeatherController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/data")
@@ -17,10 +19,19 @@ public class WeatherController {
     @RequestMapping(method=RequestMethod.GET)
     public @ResponseBody
     List<Weather> getWeather(@RequestParam(value="name", required=false, defaultValue="Moscow") String name) {
+
         Iterable<Weather> weathers = weatherRepository.findAll();
         List<Weather> list = new ArrayList<>();
         weathers.forEach(list::add);
-        return list;
+        var count = 0;
+        Collections.reverse(list);
+        List<Weather> res = new ArrayList<>();
+        for (var i: list) {
+            if (Objects.equals(i.getCity(), name)&&count<3){
+                res.add(i);
+                count++;
+            }
+        }
+        return res;
     }
-
 }
